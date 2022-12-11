510dd1abfdb906911fee4691068012d14263b6e1
breadworshipper <112392327+breadworshipper@users.noreply.github.com>
2022-12-11 21:59:15 +0700

    profilepage done without pp

--- donobox/lib/pages/profile.dart
+++ donobox/lib/pages/profile.dart
@@ -92,26 +92,178 @@ class _ProfilePageState extends State<ProfilePage> {
                       } else {
                         return ListView.builder(
                             itemCount: snapshot.data!.length,
-                            itemBuilder: (_, index) => InkWell(
-                                child: Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        horizontal: 16, vertical: 12),
-                                    padding: const EdgeInsets.all(20.0),
-                                    decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(10.0),
+                            itemBuilder: (_, index) =>
+                            Column(
+                              children: [
+                                if (snapshot.data![index].fields.bio != null)
+                                    Text("'${snapshot.data![index].fields.bio}'",
+                                      style: TextStyle(
+                                          color: Color(0xFF879999),
+                                          letterSpacing: 2
+                                      ),
                                     ),
-                                    child: Row(
-                                      children: [
-                                        Text("${snapshot.data![index].fields.role}",
-                                            style: const TextStyle(
-                                              fontSize: 18.0,
-                                              fontWeight: FontWeight.bold,
-                                            )),
-                                      ],
-                                    )
-                                )
+                                    SizedBox(height: 40),
+
+                                Text(
+                                  'Saldo',
+                                  style: TextStyle(
+                                      color: Color(0xFF879999),
+                                      letterSpacing: 2
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Rp.${snapshot.data![index].fields.saldo}",
+                                      style: TextStyle(
+                                          color: Color(0xFF879999),
+                                          letterSpacing: 1,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 40),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.people,
+                                      color: Color(0xFF879999),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      "${snapshot.data![index].fields.role}",
+                                      style: TextStyle(
+                                          color: Color(0xFF879999),
+                                          fontSize: 18,
+                                          letterSpacing: 1
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 20),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.email,
+                                      color: Color(0xFF879999),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    if (snapshot.data![index].fields.email != null)
+                                      Text(
+                                        "${snapshot.data![index].fields.email}",
+                                        style: TextStyle(
+                                            color: Color(0xFF879999),
+                                            fontSize: 18,
+                                            letterSpacing: 1
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                                SizedBox(height: 20),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.cake,
+                                      color: Color(0xFF879999),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    if (snapshot.data![index].fields.birthday != null)
+                                      Text(
+                                        "${snapshot.data![index].fields.birthday}",
+                                        style: TextStyle(
+                                            color: Color(0xFF879999),
+                                            fontSize: 18,
+                                            letterSpacing: 1
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                                SizedBox(height: 20),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.phone,
+                                      color: Color(0xFF879999),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    if (snapshot.data![index].fields.phone != null)
+                                      Text(
+                                        "${snapshot.data![index].fields.phone}",
+                                        style: TextStyle(
+                                            color: Color(0xFF879999),
+                                            fontSize: 18,
+                                            letterSpacing: 1
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                                SizedBox(height: 100),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    FloatingActionButton.extended(
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => const EditProfilePage()),
+                                        );
+                                      },
+                                      heroTag: 'editprofile',
+                                      elevation: 0,
+                                      label: const Text("Edit Profile"),
+                                      icon: const Icon(Icons.person_add_alt_1),
+                                        backgroundColor: Color(0xFF3F4E4F),
+                                    ),
+                                    const SizedBox(width: 16.0),
+                                    FloatingActionButton.extended(
+                                      onPressed: () {},
+                                      heroTag: 'mesage',
+                                      elevation: 0,
+                                      backgroundColor: Color(0xFF3F4E4F),
+                                      label: const Text("Tambah Saldo"),
+                                      icon: const Icon(Icons.money),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             )
+                            //     InkWell(
+                            //     child: Container(
+                            //         margin: const EdgeInsets.symmetric(
+                            //             horizontal: 16, vertical: 12),
+                            //         padding: const EdgeInsets.all(20.0),
+                            //         decoration: BoxDecoration(
+                            //           color: Colors.white,
+                            //           borderRadius: BorderRadius.circular(10.0),
+                            //         ),
+                            //         child: Row(
+                            //           children: [
+                            //             Text("${snapshot.data![index].fields.role}",
+                            //                 style: const TextStyle(
+                            //                   fontSize: 18.0,
+                            //                   fontWeight: FontWeight.bold,
+                            //                 )),
+                            //           ],
+                            //         )
+                            //     )
+                            // )
                         );
                       }
                     }
