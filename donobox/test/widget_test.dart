626e29bc38c10e93c252814cbc2602f3a259d157
breadworshipper <112392327+breadworshipper@users.noreply.github.com>
2022-12-11 12:56:29 +0700

    mau merge main ke branch

--- donobox/test/widget_test.dart
+++ donobox/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:donobox/main.dart';
+import 'package:donobox/pages/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
